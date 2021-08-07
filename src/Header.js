ce5ce8b2aa25753d75fed540c32d67b2c04c19b7
rishik-m <rishik.makhija2000@gmail.com>
2021-08-07 13:32:31 +0530

    header update

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ViewHeadlineIcon from '@material-ui/icons/ViewHeadline';
+import { Avatar } from '@material-ui/core';
 
 function Header() {
     return (
@@ -9,6 +13,18 @@ function Header() {
               src= {process.env.PUBLIC_URL + "/logo.png"}
               alt= "" 
             />
+            <div className= "header_cent">
+              <input type= "text" placeholder= "Start your search" />
+              <SearchIcon />
+            </div>
+            <div className= "header_right">
+              <p> Become a host </p>
+              <LanguageIcon />
+              <div className= "header_right-two">
+                <ViewHeadlineIcon />
+                <Avatar />
+              </div>
+            </div>
         </div>
     )
 }
