4a97eda69f7c4945646131d6f3813f7a0c85c116
rishik-m <rishik.makhija2000@gmail.com>
2021-08-08 03:03:57 +0530

    banner updates

--- src/Banner.js
+++ src/Banner.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css';
 import Button from '@material-ui/core/Button';
 
 function Banner() {
+
+    const [search, setSearch] = useState(false);
+
+    function handleClick() {
+        setSearch(true);
+    }
+
     return (
         <div className= "banner">
+           <div className= "search_date">
+                {search && <h1> Show Date Picker </h1>}
+                <Button onClick= {handleClick} className= "search_date-button" variant= "outlined">
+                    Search Dates
+                </Button>
+           </div>
            <div className= "banner-info">
-              <h1> Not sure where to go? Perfect.</h1>
-              <h5> Unique activities with local experts – in person or online. </h5>
-              <Button variant= "outlined"> Explore Places </Button>
+                <h1> Not sure where to go? Perfect.</h1>
+                <h5> Unique activities with local experts – in person or online. </h5>
+                <Button variant= "outlined"> Explore Places </Button>
            </div>
         </div>
     )
--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,6 @@ import './Home.css';
 function Home() {
     return (
         <div>
-          <h1> Home </h1>
           <Banner />            
         </div>
     )
