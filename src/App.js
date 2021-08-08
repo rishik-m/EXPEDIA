9ae9724f74955b794758b06734b7c87e063256a7
rishik-m <rishik.makhija2000@gmail.com>
2021-08-08 14:39:26 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './App.css';
 import Home from './Home';
+import SearchScreen from './SearchScreen';
 import Header from './Header';
 import Footer from './Footer';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+            <Route path= "/">
+              <Home />
+            </Route>
+            <Route path= "/search">
+              <SearchScreen />
+            </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
